d26355137617f741e056ed1a7d3929f83c9e90f7
Jamie Clinton <jcclinton24@gmail.com>
2014-06-07 10:52:56 -0700

    added kd_tree tests

--- test/kd_tree_tests.erl
+++ test/kd_tree_tests.erl
@@ -10,7 +10,9 @@
 
 kd_tree_test_() ->
 [
-{setup, fun build_tree1/0, fun stop/1, fun check/1}
+{setup, fun build_tree1/0, fun stop/1, fun nn/1},
+{setup, fun build_tree2/0, fun stop/1, fun nn/1},
+{setup, fun build_tree_re/0, fun stop/1, fun rebalance/1}
 ].
 
 
@@ -21,11 +23,23 @@ kd_tree_test_() ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 build_tree1() ->
-	Points = [{1,2}],
+	Points = get_points(),
+	Point = {1,2,3,4},
 	Tree = kd_tree:create(Points),
-	Expected = Points,
-	{Tree, Expected}.
+	{Tree, Point, Point}.
 
+build_tree2() ->
+	Points = get_points(),
+	Point = {0,0,0,0},
+	Expected = {1,2,3,4},
+	Tree = kd_tree:create(Points),
+	{Tree, Point, Expected}.
+
+
+build_tree_re() ->
+	Points = get_points(),
+	Tree = kd_tree:create(Points),
+	{Tree}.
 
 
 
@@ -36,11 +50,23 @@ stop(_SetupData) ->
 
 
 
+%%%%%%%%%%%%%%%%%%
+%% helper functions
+%%%%%%%%%%%%%%%%%%
+get_points() ->
+	lists:map(fun(K) ->
+		{K, K+1,K+2,K+3}
+	end, lists:seq(1,20)).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Actual Tests %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-check({_Tree, ExpectedResult}) ->
-	%Result = kd_tree:query_range(Range, Quad),
-	Result = true,
+nn({Tree, Point, ExpectedResult}) ->
+	Result = kd_tree:nearest_neighbor(Point, Tree),
 	[?_assertEqual(ExpectedResult, Result)].
+
+rebalance({Tree}) ->
+	Result = kd_tree:rebalance(Tree),
+	[?_assertEqual(Tree, Result)].
