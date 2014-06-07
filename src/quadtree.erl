c0368bc6372c40ad30d61ab68e737a9ffc1c588d
Jamie Clinton <jcclinton24@gmail.com>
2014-06-06 22:45:22 -0700

    cleaned up quadtree data structure

--- src/quadtree.erl
+++ src/quadtree.erl
@@ -1,18 +1,25 @@
 -module(quadtree).
 
--export([new/2, insert/2, query_range/2]).
+-export([new/1, insert/2, query_range/2]).
 
 
-new(Center, HalfDimension) -> {quad, Center, HalfDimension, [], null, null, null, null}.
+new({{CenterX, CenterY}, HalfDimension}) when is_number(CenterX), is_number(CenterY), is_number(HalfDimension) ->
+	Range = {{CenterX, CenterY}, HalfDimension},
+	Points = [],
+	Regions = {null, null, null, null},
+	{quad, Range, Points, Regions};
+new(_) -> {error, badarg}.
 
-insert(Point, Quad) ->
-	{quad, Center, HalfDimension, _Points, _NW, _NE, _SW, _SE} = Quad,
-	IsBound = is_bound(Point, {Center, HalfDimension}),
+insert(Point, Quad) when is_tuple(Point), is_tuple(Quad) ->
+	{quad, Range, _, _} = Quad,
+	IsBound = is_bound(Point, Range),
 	if IsBound ->
 			append(Point, Quad);
 		IsBound == false ->
 			{false, Quad}
-	end.
+	end;
+insert(_, _) -> {error, badarg}.
+
 
 is_bound({X, Y}, {{CenterX, CenterY}, HalfDimension}) ->
 	EastEdge = CenterX + HalfDimension,
@@ -25,64 +32,66 @@ is_bound({X, Y}, {{CenterX, CenterY}, HalfDimension}) ->
 
 
 append(Point, Quad) ->
-	{quad, Center, HalfDimension, Points, NW, NE, SW, SE} = Quad,
+	{quad, Range, Points, Regions} = Quad,
 	if length(Points) < 4 ->
 			NewPoints = [Point|Points],
-			NewQuad = {quad, Center, HalfDimension, NewPoints, NW, NE, SW, SE},
+			NewQuad = {quad, Range, NewPoints, Regions},
 			{true, NewQuad};
 		true ->
 			subdivide_insert(Point, Quad)
 		end.
 	
 subdivide_insert(Point, Quad) ->
-	{quad, _, _, _, OldNW, _, _, _} = Quad,
+	{quad, _, _, {OldNW, _, _, _}} = Quad,
 	DividedQuad = if OldNW == null -> subdivide(Quad);
 							true -> Quad
 						end,
-	{quad, Center, HalfDimension, Points, NW, NE, SW, SE} = DividedQuad,
+	{quad, Range, Points, {NW, NE, SW, SE}} = DividedQuad,
 	% not efficient to call all 4, but should work
 	{_, NewNW} = insert(Point, NW),
 	{_, NewNE} = insert(Point, NE),
 	{_, NewSW} = insert(Point, SW),
 	{_, NewSE} = insert(Point, SE),
-	NewQuad = {quad, Center, HalfDimension, Points, NewNW, NewNE, NewSW, NewSE},
+	NewQuad = {quad, Range, Points, {NewNW, NewNE, NewSW, NewSE}},
 	{true, NewQuad}.
 
 
 
 subdivide(Quad) ->
-	{quad, {CenterX, CenterY}, HalfDimension, Points, null, null, null, null} = Quad,
-	NewHalf = HalfDimension div 2,
+	{quad, Range, Points, {null, null, null, null}} = Quad,
+	{{CenterX, CenterY}, HalfDimension} = Range,
+	NewHalf = HalfDimension / 2,
 	NWCenter = {CenterX - NewHalf, CenterY + NewHalf},
 	NECenter = {CenterX + NewHalf, CenterY + NewHalf},
 	SWCenter = {CenterX - NewHalf, CenterY - NewHalf},
 	SECenter = {CenterX + NewHalf, CenterY - NewHalf},
-	NW = new(NWCenter, NewHalf),
-	NE = new(NECenter, NewHalf),
-	SW = new(SWCenter, NewHalf),
-	SE = new(SECenter, NewHalf),
-	{quad, {CenterX, CenterY}, HalfDimension, Points, NW, NE, SW, SE}.
+	NW = new({NWCenter, NewHalf}),
+	NE = new({NECenter, NewHalf}),
+	SW = new({SWCenter, NewHalf}),
+	SE = new({SECenter, NewHalf}),
+	{quad, Range, Points, {NW, NE, SW, SE}}.
 
 
-query_range(Range, Quad) ->
-	{quad, Center, HalfDimension, _, _, _, _, _} = Quad,
-	Intersects = intersects({Center, HalfDimension}, Range),
+query_range(InputRange, Quad) when is_tuple(InputRange), is_tuple(Quad) ->
+	{quad, Range, _, _} = Quad,
+	Intersects = intersects(Range, InputRange),
 	if Intersects ->
-			query_range(Range, Quad, []);
+			query_range(InputRange, Quad, []);
 		true -> []
-	end.
+	end;
+query_range(_, _) -> {error, badarg}.
 
-query_range(Range, Quad, Acc) ->
-	{quad, _, _, Points, NW, NE, SW, SE} = Quad,
+query_range(InputRange, Quad, Acc) ->
+	{quad, _, Points, {NW, NE, SW, SE}} = Quad,
 	PointsAcc = lists:foldl(fun(Point, AccIn) ->
-								InRange = is_bound(Point, Range),
-								if InRange -> [Point|AccIn];
+								IsInRange = is_bound(Point, InputRange),
+								if IsInRange -> [Point|AccIn];
 									true -> AccIn
 								end
 							end, Acc, Points),
 	lists:foldl(fun(QuadIn, AccIn) ->
 								if QuadIn == null -> AccIn;
-									true -> AccIn ++ query_range(Range, QuadIn)
+									true -> AccIn ++ query_range(InputRange, QuadIn)
 								end
 							end, PointsAcc, [NW, NE, SW, SE]).
 
