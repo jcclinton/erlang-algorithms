6df96d7de805d39e2f24c00582c210722186a784
Jamie Clinton <jcclinton24@gmail.com>
2014-06-08 13:16:38 -0700

    added comments to functions

--- src/dijkstra.erl
+++ src/dijkstra.erl
@@ -1,7 +1,6 @@
 -module(dijkstra).
 
--export([]).
--compile([export_all]).
+-export([run/3, test/0]).
 
 
 
@@ -44,45 +43,36 @@ get_node_value(Node, Value) ->
 
 %% graph functions
 
-create_graph() ->
-	Starting = 1,
-	Funs = [fun bottom_check/2, fun top_check/2, fun left_check/2, fun right_check/2],
+%% creates a square grid graph
+create_graph(StartId, SideLength) ->
+	Size = SideLength * SideLength,
+	Funs = [fun bottom_check/3, fun top_check/3, fun left_check/3, fun right_check/3],
 	lists:foldr(fun(N, Graph) ->
 		Conns = lists:foldr(fun(Fun, Nodes) ->
-							Fun(N, Nodes)
+							Fun(N, Nodes, SideLength)
 						end, [], Funs),
-		Distance = if Starting == N -> 0; true -> infinity end,
+		Distance = if StartId == N -> 0; true -> infinity end,
 		Node = create_node(N, Distance, Conns),
 		[Node|Graph]
-	end, [], lists:seq(1, 100)).
+	end, [], lists:seq(1, Size)).
 
-bottom_check(N, Nodes) ->
-	if N < 11 -> Nodes;
-		true -> [N-10|Nodes]
+bottom_check(N, Nodes, SideLength) ->
+	if N < SideLength + 1 -> Nodes;
+		true -> [N-SideLength|Nodes]
 	end.
-top_check(N, Nodes) ->
-	if N > 90 -> Nodes;
-		true -> [N+10|Nodes]
+top_check(N, Nodes, SideLength) ->
+	TopRowStart = SideLength * SideLength - SideLength,
+	if N > TopRowStart -> Nodes;
+		true -> [N+SideLength|Nodes]
 	end.
-left_check(N, Nodes) ->
-	if N rem 10 == 1 -> Nodes;
+left_check(N, Nodes, SideLength) ->
+	if N rem SideLength == 1 -> Nodes;
 		true -> [N-1|Nodes]
 	end.
-right_check(N, Nodes) ->
-	if N rem 10 == 0 -> Nodes;
+right_check(N, Nodes, SideLength) ->
+	if N rem SideLength == 0 -> Nodes;
 		true -> [N+1|Nodes]
 	end.
-	
-
-sort_graph(Graph) ->
-	lists:sort(fun(A, B) ->
-		AId = get_node_id(A),
-		BId = get_node_id(B),
-		if AId =< BId -> true;
-			true -> false
-		end
-	end, Graph).
-		
 
 %% end graph functions
 
@@ -90,17 +80,39 @@ sort_graph(Graph) ->
 %% public api
 
 test() ->
-	run(1, 100, create_graph()).
+	Start = 1,
+	SideLength = 20,
+	io:format("creating graph with ~p nodes~n", [SideLength*SideLength]),
+	End = SideLength * SideLength,
+	Graph = create_graph(Start, SideLength),
+	OutGraph = run(Start, End, Graph),
+	case OutGraph of
+		no_connection ->
+			io:format("no connection from start to finish was found in this graph~n");
+		_ ->
+			Path = get_path(Start, End, OutGraph),
+			io:format("final path starting at ~p and ending at ~p: ~p~n", [Start, End, Path])
+	end,
+	ok.
 
+
+% runs the algorithm
+% returns the modified graph as output
 run(StartId, EndId, Graph) ->
 	CurrentNode = get_node(Graph, StartId),
-	OutGraph = check_current_node(Graph, CurrentNode, EndId),
-	%SortedOutGraph = sort_graph(OutGraph),
-	%io:format("out graph: ~p~n", [SortedOutGraph]),
-	get_path(StartId, EndId, OutGraph).
+	check_current_node(Graph, CurrentNode, EndId).
+
 
 %% private functions
 
+% runs algorithm for given node
+% -finds distance to all neighbors through this node, if less than neighbors current length, replace neighbors distance with new value
+% -take unvisited node with lowest distance as the next node to check
+% repeat until end conditions are met
+% algorithm ends when endid is mark visited or there are no unvisited nodes with finite distance
+
+% should never get to the end of the graph, either of the end conditions will be met first
+check_current_node([], _CurrentNode, _EndId) -> no_connection;
 check_current_node(Graph, CurrentNode, EndId) ->
 	CurrId = get_node_id(CurrentNode),
 	Distance = get_node_distance(CurrentNode),
@@ -129,6 +141,7 @@ check_current_node(Graph, CurrentNode, EndId) ->
 			end
 	end.
 
+% returns an unvisited node with the lowest distance
 get_next_node(Nodes) ->
 	get_next_node(Nodes, []).
 
@@ -151,18 +164,10 @@ get_next_node([Node|Rest], Next) ->
 						end,
 	get_next_node(Rest, NewNext).
 
-	
 
 
-remove_node(RemoveNode, Nodes) ->
-	RemoveId = get_node_id(RemoveNode),
-	lists:foldl(fun(Node, AccIn) ->
-								Id = get_node_id(Node),
-								if RemoveId == Id -> AccIn;
-									true -> [Node|AccIn]
-								end
-							end, [], Nodes).
-
+% replaces a node in the graph
+% used to mark a node as visited
 replace_node(Node, Graph) ->
 	replace_node(Node, Graph, []).
 replace_node(_Node, [], Acc) -> Acc;
@@ -176,20 +181,9 @@ replace_node(Node, [Head|Tail], Acc) ->
 	end.
 
 
-replace_nodes(Nodes, Graph) ->
-	lists:foldl(fun(GraphNode, GraphAcc) ->
-													Id = get_node_id(GraphNode),
-													NewNode = lists:foldl(fun(ReplaceNode, AccIn) ->
-														ReplaceId = get_node_id(ReplaceNode),
-														if Id == ReplaceId -> ReplaceNode;
-															true -> AccIn
-														end
-													end, GraphNode, Nodes),
-													[NewNode|GraphAcc]
-												end, [], Graph).
 
 
-
+% looks up node by id
 get_node([], _) -> throw(badarg);
 get_node([Node|Graph], LookupId) ->
 	Id = get_node_id(Node),
@@ -197,6 +191,11 @@ get_node([Node|Graph], LookupId) ->
 		true -> get_node(Graph, LookupId)
 	end.
 
+% calculates tentative distance
+% tentative distance is the distance from node A to origin via Node B
+% if node A's original distance is smaller than the tentative distance,
+% node A will keep its original value
+% once a node has been visited, its distance will no longer change
 calc_tentative_distance(Nodes, CurrDistance) ->
 	calc_tentative_distance(Nodes, CurrDistance, []).
 calc_tentative_distance([], _CurrDistance, Acc) -> Acc;
@@ -221,22 +220,24 @@ calc_tentative_distance([Node|Rest], CurrDistance, Acc) ->
 
 %% path functions
 
+% returns a list of ids that constitutes the shortest path from start to end in this graph
+% starts at the end node and finds the shortest path back to the start node,
+% which just consists of picking the neighbor with the shortest distance
 get_path(StartId, EndId, Graph) ->
 	EndNode = get_node(Graph, EndId),
-	get_path_list(EndNode, Graph, StartId).
+	get_path(EndNode, Graph, StartId, []).
 
-get_path_list(CurrNode, Graph, StartId) ->
-	get_path_list(CurrNode, Graph, StartId, []).
-
-get_path_list(CurrNode, Graph, StartId, Ids) ->
+get_path(CurrNode, Graph, StartId, Ids) ->
 	CurrId = get_node_id(CurrNode),
 	Conns = get_node_connected(CurrNode),
-	if CurrId == StartId -> Ids;
+	if CurrId == StartId -> [StartId|Ids];
 		true ->
 			NextNode = get_shortest_conn_node(Conns, Graph),
-			get_path_list(NextNode, Graph, StartId, [CurrId|Ids])
+			get_path(NextNode, Graph, StartId, [CurrId|Ids])
 	end.
 
+% returns the neighbor with the shortest distance back to the origin
+% this will be the next node back in the path to the origin
 get_shortest_conn_node(Conns, Graph) ->
 	Empty = null,
 	lists:foldl(fun(ConnId, ShortestNode) ->
