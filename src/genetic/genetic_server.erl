fe725eb23d2d9d0a5df7432ac2ffc82592cfa89a
Jamie Clinton <jcclinton24@gmail.com>
2014-06-09 16:59:54 -0700

    fixed bug with mutate function

--- src/genetic/genetic_server.erl
+++ src/genetic/genetic_server.erl
@@ -6,7 +6,8 @@
 								worker_sup_pid,
 								remaining,
 								results,
-								target
+								target,
+								generation
 							 }).
 
 
@@ -16,6 +17,7 @@
 
 % size of our chromosome in bytes
 -define(Size, 4).
+-define(MutationChance, 100).
 
 
 start_link(ParentPid) ->
@@ -50,22 +52,33 @@ handle_cast({success, Result}, State=#state{remaining=Remaining, n=N, results=Re
 		true -> CombinedResults
 	end,
 	{noreply, State#state{remaining=NewRemaining, results=NewResults}};
-handle_cast({breed, Results}, State=#state{target=Target}) ->
+handle_cast({breed, Results}, State=#state{target=Target, generation=Generation}) ->
+	End = 1000,
 	Done = lists:foldl(fun({Num, _}, Bool) ->
+			%io:format("number: ~p~n", [Num]),
 							if Bool -> Bool;
 								Num == Target -> true;
 								true -> false
 							end
 				end, false, Results),
 	if Done ->
-			io:format("~p found~n", [Target]);
+			io:format("~p found after ~p generations~n", [Target, Generation]);
+		Generation > End ->
+			io:format("did not find after ~p generations~n", [Generation]);
 		true ->
-			io:format("input parents: ~p~n", [Results]),
+			%io:format("input parents: ~p~n", [Results]),
 			Fitness = get_fitness(Results, Target),
+			Total = get_fitness_total(Fitness),
+			io:format("breeding generation ~p with total fitness: ~p~n", [Generation, Total]),
 			Children = breed(Fitness),
-			io:format("output children: ~p~n", [Children]),
-			ok
+			gen_server:cast(self(), {next_step, Children})
+			%io:format("output children: ~p~n", [Children]),
 	end,
+	{noreply, State#state{generation=Generation+1, results=[], remaining=0}};
+handle_cast({next_step, Children}, State=#state{worker_sup_pid=Pid}) ->
+	lists:foreach(fun(Bytes) ->
+		supervisor:start_child(Pid, [Bytes])
+	end, Children),
 	{noreply, State#state{}};
 handle_cast(create_first_gen, State=#state{worker_sup_pid=Pid, n=N}) ->
 	lists:foreach(fun(_) ->
@@ -75,7 +88,7 @@ handle_cast(create_first_gen, State=#state{worker_sup_pid=Pid, n=N}) ->
 		Bytes = <<15:4/integer, First:4/integer, Rest/binary>>,
 		supervisor:start_child(Pid, [Bytes])
 	end, lists:seq(1, N)),
-	{noreply, State#state{remaining=0, results=[]}};
+	{noreply, State#state{remaining=0, results=[], generation=0}};
 handle_cast(Msg, State) ->
 	io:format("genetic SERVER: received unknown cast: ~p~n", [Msg]),
 	{noreply, State}.
@@ -96,82 +109,84 @@ terminate(_Reason, _State) ->
 
 %% private
 
+% calculates fitness from the calculated results
 get_fitness(Results, Target) ->
 	get_fitness(Results, Target, []).
-	
 get_fitness([], _Target, Acc) -> Acc;
 get_fitness([{Num, Bytes}|Rest], Target, Acc) ->
 	Fitness = erlang:round(10000 * erlang:abs(1 / (Target - Num))),
 	get_fitness(Rest, Target, [{Fitness, Bytes}|Acc]).
 
 
+% takes fitness list and data, then selects parents, breeds and mutates children
 breed(Fitnesses) ->
 	Length = length(Fitnesses),
-	Children = lists:foldl(fun(_, AccIn) ->
+	Parents = lists:foldl(fun(_, AccIn) ->
 													{Fitness1, Fitnesses1} = select(Fitnesses),
 													{Fitness2, _} = select(Fitnesses1),
 													%io:format("out of all fitnesses: ~p~nselected 1: ~p~nselected 2: ~p~n", [Fitnesses, Fitness1, Fitness2]).
 													[{Fitness1, Fitness2} | AccIn]
 											end, [], lists:seq(1, Length div 2)),
-	NewChildren = breed_new_children(Children),
+	NewChildren = breed_new_children(Parents),
 	mutate_children(NewChildren).
 
 
+% iterates through children and may mutate
 mutate_children(Children) ->
 	mutate_children(Children, []).
-
 mutate_children([], Acc) -> Acc;
 mutate_children([Child|Rest], Acc) ->
-	Chance = random:uniform(1000),
+	Chance = random:uniform(?MutationChance),
 	NewChild = if Chance == 1 ->
-		io:format("mutating: ~p~n", [Child]),
+		%io:format("mutating: ~p~n", [Child]),
 		mutate(Child);
 							true -> Child
 						end,
+		%io:format("mutated: ~p~n", [NewChild]),
 	mutate_children(Rest, [NewChild|Acc]).
 
-mutate(<<B1:8, B2:8, B3:8, B4:8, B5:8>>) ->
-	Offset = random:uniform(5),
+% mutates one byte in a 5 byte word
+% kind of ugly
+mutate(<<B1:8, B2:8, B3:8, B4:8>>) ->
+	Offset = random:uniform(?Size),
 	NewB1 = if Offset == 1 -> mutate_byte(B1);
 							true -> B1
 					end,
-	NewB2 = if Offset == 1 -> mutate_byte(B2);
+	NewB2 = if Offset == 2 -> mutate_byte(B2);
 							true -> B2
 					end,
-	NewB3 = if Offset == 1 -> mutate_byte(B3);
+	NewB3 = if Offset == 3 -> mutate_byte(B3);
 							true -> B3
 					end,
-	NewB4 = if Offset == 1 -> mutate_byte(B4);
+	NewB4 = if Offset == 4 -> mutate_byte(B4);
 							true -> B4
 					end,
-	NewB5 = if Offset == 1 -> mutate_byte(B5);
-							true -> B5
-					end,
-	<<NewB1:8, NewB2:8, NewB3:8, NewB4:8, NewB5:8>>.
+	<<NewB1:8, NewB2:8, NewB3:8, NewB4:8>>.
+
 
 %% flips one bit in a byte
 mutate_byte(Byte) ->
 	BitOffset = random:uniform(8),
 	BitMask = 1 bsl (BitOffset-1),
 	Odd = Byte band BitMask,
-	if Odd == 1 -> Byte band (not BitMask);
+	if Odd == 1 -> Byte band (bnot BitMask);
 		true -> Byte bor BitMask
 	end.
 
 
 
 
-breed_new_children(Children) ->
-	breed_new_children(Children, []).
-
+% breeds new children from selected parents
+breed_new_children(Parents) ->
+	breed_new_children(Parents, []).
 breed_new_children([], Acc) -> Acc;
 breed_new_children([{{_, ParentByte1}, {_, ParentByte2}}|Rest], Acc) ->
 	Chance = random:uniform(10),
-	{NewChild1, NewChild2} = if Chance > 3 -> create_children_pair(ParentByte1, ParentByte2);
+	{NewChild1, NewChild2} = if Chance == 1 -> create_children_pair(ParentByte1, ParentByte2);
 											true -> {ParentByte1, ParentByte2}
 										end,
-		io:format("p1: ~p p2: ~p~n", [ParentByte1, ParentByte2]),
-		io:format("c1: ~p c2: ~p~n", [NewChild1, NewChild2]),
+		%io:format("p1: ~p p2: ~p~n", [ParentByte1, ParentByte2]),
+		%io:format("c1: ~p c2: ~p~n", [NewChild1, NewChild2]),
 	breed_new_children(Rest, [NewChild1|[NewChild2|Acc]]).
 
 
@@ -188,14 +203,10 @@ create_children_pair(Bytes1, Bytes2) ->
 								if I > CrossoverLength ->
 										NewChild1 = <<Child1/binary, B2:8>>,
 										NewChild2 = <<Child2/binary, B1:8>>,
-										%NewChild1 = <<B2:8, Child1/binary>>,
-										%NewChild2 = <<B1:8, Child2/binary>>,
 										{NewChild1, NewChild2};
 									true ->
 										NewChild1 = <<Child1/binary, B1:8>>,
 										NewChild2 = <<Child2/binary, B2:8>>,
-										%NewChild1 = <<B1:8, Child1/binary>>,
-										%NewChild2 = <<B2:8, Child2/binary>>,
 										{NewChild1, NewChild2}
 								end
 						end, {<<>>, <<>>}, lists:seq(1, Length)),
@@ -203,6 +214,7 @@ create_children_pair(Bytes1, Bytes2) ->
 
 
 
+% randomly selects a byte, the higher its fitness, the higher chance it has of being selected
 select(Fitnesses) ->
 	Total = get_fitness_total(Fitnesses),
 	Random = random:uniform(Total),
@@ -224,9 +236,19 @@ select(Fitnesses) ->
 															
 
 
+% calculates the combined fitness score of all the bytes
 get_fitness_total(Fitnesses) ->
 	get_fitness_total(Fitnesses, 0).
-
 get_fitness_total([], Total) -> Total;
 get_fitness_total([{Num, _}|Rest], Total) ->
 	get_fitness_total(Rest, Total + Num).
+
+
+-define(bit, :1/unsigned-integer).
+
+iter_bits(Bytes) ->
+	iter_bits(Bytes, <<>>).
+iter_bits(<<>>, Acc) -> Acc;
+iter_bits(<<N1?bit,N2?bit,N3?bit,N4?bit,N5?bit,N6?bit,N7?bit,N8?bit, Rest/binary>>, Acc) ->
+	io:format("bit: ~p~n", [N8]),
+	iter_bits(Rest, <<Acc/binary, N1?bit, N2?bit, N3?bit, N4?bit, N5?bit, N6?bit, N7?bit, N8?bit>>).
